7ab20930a34654ab9b45ce7b1d53ef65a7ac2600
msrkreddy0928 <msrkreddy111@gmail.com>
2024-12-19 18:37:55 +0530

    APIs are created using Flask

--- src/model_training.py
+++ src/model_training.py
@@ -1,14 +1,18 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.pipeline import Pipeline
+import joblib
 
 
- 
 
 def train_model(model,X_train, y_train):
    model.fit(X_train,y_train)
    return model
     
+
+def save_model(model):
+   joblib.dump(model,'best_model.pkl')
+       
     
     
     
